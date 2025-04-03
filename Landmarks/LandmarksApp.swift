929264ed74e6b85afa493a3fd6d8bf143d8022a8
Dhruv Shah <dhruv0310@icloud.com>
2025-04-03 12:03:11 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Dhruv Shah on 03/04/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
