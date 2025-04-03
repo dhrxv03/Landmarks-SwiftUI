6df72b765739ff5e5285a766ce00e0cf5b97ac41
Dhruv Shah <dhruv0310@icloud.com>
2025-04-03 13:35:06 +0530

    Add Image Credit

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -3,7 +3,7 @@
 //  Landmarks
 //
 //  Created by Dhruv Shah on 03/04/25.
-//
+// Photo Credit to Photo by Vivek Doshi  - https://unsplash.com/photos/a-large-stone-arch-with-a-clock-on-it-zbssMg68BUo?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash
 
 import SwiftUI
 
@@ -13,6 +13,7 @@ struct ContentView: View {
         VStack {
             MapView()
                 .frame(height: 300)
+            
             
             CircleImage()
                 .offset(y: -130)
